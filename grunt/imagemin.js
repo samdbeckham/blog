0ce8ed4aa89a1ea429512744acbc26b0cd61eebb
samdbeckham <sam@drummondcentral.co.uk>
2014-12-31 12:38:48 +0000

    Added imagemin and moved the images to a more appropriate place

---
+++ grunt/imagemin.js
@@ -0,0 +1,30 @@
+module.exports = {
+    dev: {
+        options: {
+            optimizationLevel: 0
+        },
+        files: [
+            {
+                expand: true,
+                dot: true,
+                cwd: 'dev/_assets/images',
+                dest:'.tmp/images',
+                src: ['**/*.{jpg,gif,png}']
+            }
+        ]
+    },
+    dist: {
+        options: {
+            optimizationLevel: 7
+        },
+        files: [
+            {
+                expand: true,
+                dot: true,
+                cwd: '_assets/images',
+                dest:'web/images',
+                src: ['**/*.{jpg,gif,png}']
+            }
+        ]
+    }
+};
