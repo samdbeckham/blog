b92bc4a472b6771c684b7acf309aedc1165a1afc
samdbeckham <sam@beckham.io>
2015-11-08 10:15:26 +0000

    Added fitvids

--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -1,5 +1,6 @@
 var pageTransition = require('./components/pageTransition.js'),
     imageFixer = require('./components/imageFixer'),
+    fitvids = require('fitvids'),
     cards = document.getElementsByClassName('card'),
     images = document.getElementsByTagName('img'),
     i;
@@ -16,3 +17,5 @@ for (i = 0; i < cards.length; i +=1) {
 for (i = 0; i < images.length; i +=1) {
     new imageFixer(images.item(i));
 }
+
+fitvids();
