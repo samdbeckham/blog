10f442f7752fec17a8ede7e9f98b040b258c8ae9
samdbeckham <sam@beckham.io>
2016-06-26 12:44:46 +0100

    linted all the files

--- dev/_assets/scripts/serviceWorker.js
+++ dev/_assets/scripts/serviceWorker.js
@@ -1,4 +1,4 @@
-/*global self: true, caches: true, Response: true */
+/*global self: true, caches: true */
 'use strict';
 
 const expectedCaches = ['static-v1'];
@@ -9,18 +9,18 @@ self.addEventListener('fetch', event => {
     if(url.pathname.endsWith('html') &&
         url.pathname.startsWith('/wrote')) {
         caches.open('static-v1')
-            .then(cache => cache.add(url))
+            .then(cache => cache.add(url));
     }
 
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
             .catch(() => {
-                if (event.request.mode == 'navigate') {
-                    return caches.match('/offline.html')
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/offline.html');
                 }
             })
-    )
+    );
 });
 
 self.addEventListener('install', event => {
@@ -33,7 +33,7 @@ self.addEventListener('install', event => {
                 '/wrote',
                 '/offline.html'
             ]))
-    )
+    );
 });
 
 self.addEventListener('activate', event => {
