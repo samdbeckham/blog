da95400b5bbb9e82173fe82b2e9d9bb9c683aee2
sambeckham <sam.beckham@bytemark.co.uk>
2016-07-19 13:53:24 +0100

    :construction_worker: Fixes modernizr again

--- grunt/modernizr.js
+++ grunt/modernizr.js
@@ -1,11 +1,13 @@
 module.exports = {
-    devFile: 'node_modules/npm-modernizr/modernizr.js',
-    outputFile: 'web/scripts/vendor/modernizr.js',
-    files: {
-        src: [
-            'dev/_assets/scripts/{,*/}*.js',
-            'dev/_assets/scss/{,*/}*.scss'
-        ]
-    },
-    uglify: true
+    dist: {
+        devFile: 'node_modules/npm-modernizr/modernizr.js',
+        outputFile: 'web/scripts/vendor/modernizr.js',
+        files: {
+            src: [
+                'dev/_assets/scripts/{,*/}*.js',
+                'dev/_assets/scss/{,*/}*.scss'
+            ]
+        },
+        uglify: true
+    }
 };
