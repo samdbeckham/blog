9a9ae055bad2b8e02d1b263e68dea64ad9b60589
sambeckham <sam.beckham@bytemark.co.uk>
2016-07-19 14:00:44 +0100

    :heavy_plus_sign: Adds auto commiting to bump

--- grunt/bump.js
+++ grunt/bump.js
@@ -1,7 +1,9 @@
 module.exports = {
     options: {
         files: ['package.json', 'dev/_assets/scripts/serviceWorker.js'],
-        commit: false,
+        commit: true,
+        commitFiles: ['package.json', 'dev/_assets/scripts/serviceWorker.js'],
+        commitMessage: ':shipit: Release %VERSION%',
         createTag: false,
         push: false
     }
