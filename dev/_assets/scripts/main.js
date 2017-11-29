9a2adead49220a51308e65f3d37be2f262a0f08a
sambeckham <sam.beckham@bytemark.co.uk>
2017-11-29 23:06:37 +0000

    :notebook: Linted the files

--- dev/_assets/scripts/main.js
+++ dev/_assets/scripts/main.js
@@ -1,19 +1,19 @@
-var pageTransition = require("./components/pageTransition.js"),
-  imageFixer = require("./components/imageFixer"),
-  fitvids = require("fitvids"),
-  cards = document.getElementsByClassName("card"),
-  images = document.getElementsByTagName("img"),
+var pageTransition = require('./components/pageTransition.js'),
+  imageFixer = require('./components/imageFixer'),
+  fitvids = require('fitvids'),
+  cards = document.getElementsByClassName('card'),
+  images = document.getElementsByTagName('img'),
   i;
 
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/serviceWorker.js", {
-    scope: "/"
+  navigator.serviceWorker.register('/serviceWorker.js', {
+    scope: '/'
   });
 }
 
 setTimeout(function() {
-  "use strict";
-  document.getElementsByTagName("main")[0].classList.remove("animate-in");
+  'use strict';
+  document.getElementsByTagName('main')[0].classList.remove('animate-in');
 }, 300);
 
 for (i = 0; i < cards.length; i += 1) {
@@ -27,12 +27,13 @@ for (i = 0; i < images.length; i += 1) {
 fitvids();
 
 window.onpageshow = function(event) {
+  'use strict';
   if (event.persisted) {
-    var activeCard = document.querySelector(".card.transitioning");
+    var activeCard = document.querySelector('.card.transitioning');
     var background = activeCard.style.background;
 
-    activeCard.removeAttribute("style");
-    activeCard.classList.remove("transitioning");
+    activeCard.removeAttribute('style');
+    activeCard.classList.remove('transitioning');
     activeCard.style.background = background;
   }
 };
